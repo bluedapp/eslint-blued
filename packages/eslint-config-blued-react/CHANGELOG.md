5b8921e1b59c22b181fcbf2295846c6be2c9ce01
jiashunming <jiashunming@outlook.com>
2018-08-21 16:42:53 +0800

    update: change supports

--- packages/eslint-config-blued-react/CHANGELOG.md
+++ packages/eslint-config-blued-react/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.6
+
+update `eslint-config-blued` version, add `airbnb` supports.
+
 ## 0.0.4
 
 fix: remove `ts` dependencies.
--- packages/eslint-config-blued-react/index.js
+++ packages/eslint-config-blued-react/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   "extends": [
     "eslint-config-blued",
+    "eslint-config-airbnb/rules/react",
+    "eslint-config-airbnb/rules/react-a11y",
     "./rules/import.yml",
     "./rules/react.yml"
   ].map(require.resolve),
